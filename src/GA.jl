50c18e294558a17662961cca27533bab5dd8425a
Magnus Urquhart <magnus.urquhart@gmail.com>
2018-04-26 14:41:19 +0200

    Updated Require. Split function to separate file. Update export

---
+++ src/GA.jl
@@ -0,0 +1,165 @@
+"""
+    function mutateLHC!(LHC)
+Perform one random mutation to the LHC. Interchange two randomly selected elements
+from a randomly selected column.
+"""
+function mutateLHC!(LHC)
+
+    n, d = size(LHC) #Num points, num dimensions
+
+    col = sample(1:d) #Randomly choosen column
+    ind1 ,ind2 =  sample(1:n, 2, replace = false) #Two unique & random indices
+
+    #Swap elements
+    tmp = LHC[ind1,col]
+    LHC[ind1,col] = LHC[ind2,col]
+    LHC[ind2,col] = tmp
+
+    return
+
+end
+
+
+
+"""
+    function tournament(pop,numtour::Int,prob::Int)
+Choose `numtour` random, unique, individuals and choose the best individual
+with probability `prob`. Return selected individual index
+"""
+function tournament(popfit::Vector{Float64},numtour::Int,prob)
+
+    p = length(popfit)
+
+    #Unique individuals for tournament
+    tourInds = sample(1:p, numtour, replace = false)
+
+    #Fitness among contenders sorted in fitInds
+    fitnesses = popfit[tourInds]
+    fitInds = sortperm(fitnesses,rev=true)
+
+    #Return the selected index of the individual
+    for i = 1:numtour-1
+        if rand() <= prob
+            fittest = fitInds[i]
+            return tourInds[fittest]    #Winner!
+        end
+    end
+    return tourInds[fitInds[end]]       #Lucky chum!
+
+end
+
+
+
+"""
+    function _cyclecross(parone,partwo)
+Cyclecrossover of two parents to create one offspring.
+"""
+function _cyclecross(parone::Vector,partwo::Vector)
+
+    #initialise offspring
+    offspr = Vector{typeof(parone[1])}(length(parone))
+    visited = BitArray(length(parone)).=false
+
+    #first value is direct copy of the first parent
+    offspr[1] = parone[1]
+    ind = 1
+
+    #loop over values until all possible visits are made
+    while visited[ind] == false
+      visited[ind] = true
+      ind = findfirst(parone, partwo[ind])
+      offspr[ind] = parone[ind]
+    end
+
+    #use remaining values from the second parent
+    flipbits!(visited)
+    offspr[visited] .= partwo[visited]
+
+    return offspr
+
+end
+
+
+
+"""
+    function cyclecross(parone,partwo)
+Cyclecrossover of two parents to create two offspring.
+"""
+function cyclecross(parone::Vector,partwo::Vector)
+
+    offsprone = _cyclecross(parone,partwo)
+    offsprtwo = _cyclecross(partwo,parone)
+
+    return offsprone, offsprtwo
+
+end
+
+
+
+"""
+    function _fixedcross(parone,partwo)
+Fixed point crossover of two parents to create one offspring.
+"""
+function _fixedcross(parone::Vector,partwo::Vector)
+
+    #initialise offspring
+    offspr = zeros(typeof(parone[1]),length(parone))
+
+    #generate a random location in the gene
+    n = length(parone)
+    loc = sample(1:n-1)
+
+    offspr[1:loc] = parone[1:loc]
+    i = loc+1
+    while i < n+1
+        for j = 1:n
+            x = findfirst(offspr,partwo[j])
+            if x == 0 && offspr[i] == 0
+                offspr[i] = partwo[j]
+                i += 1
+            end
+        end
+    end
+
+    return offspr
+
+end
+
+
+
+"""
+    function fixedcross(parone,partwo)
+Fixed point crossover of two parents to create two offspring.
+"""
+function fixedcross(parone::Vector,partwo::Vector)
+
+    offsprone = _fixedcross(parone,partwo)
+    offsprtwo = _fixedcross(partwo,parone)
+
+    return offsprone, offsprtwo
+
+end
+
+
+
+"""
+    function inversion!(individual)
+Reverse the values in a gene between two random cut-off points.
+"""
+function inversion!(individual)
+
+    #generate two, unique, random locations in the gene
+    n = length(individual)
+    invLocs = sample(1:n, 2, replace = false)
+    sort!(invLocs)
+
+    #range of inversion
+    invRange = invLocs[1]:invLocs[2]
+
+    #flip the values in the range and assign them to the vector in place
+    tmp = flipdim(individual[invRange],1)
+    individual[invRange] = tmp
+
+    return
+
+end
--- src/LatinHypercubeSampling.jl
+++ src/LatinHypercubeSampling.jl
@@ -1,5 +1,362 @@
 module LatinHypercubeSampling
 
-# package code goes here
+export  randomLHC,
+        AudzeEgliasObjective,
+        LHCoptim,
+        LHCoptim!,
+        subLHCoptim,
+        subLHCindex,
+        refineLHCoptim,
+
+
+
+include("GA.jl")
+
+
+using StatsBase
+
+"""
+    function randomLHC(n::Int, d::Int)
+Generate a random Latin Hypercube with `d` dimensions and `n` sample points.
+"""
+function randomLHC(n::Int,d::Int)
+
+    LHC = Array{Int}(n,d)
+    for i = 1:d
+        LHC[:,i] = randperm(n)
+    end
+
+    return LHC
+
+end
+
+
+
+"""
+    function AudzeEgliasObjective(dist,LHC::Array)
+Return the scalar which should be maximized when using the Audze-Eglias
+distance as the objective function. Note this is the inverse of the typical
+Audze-Eglias distance which normally is minimized.
+"""
+function AudzeEgliasObjective(dist,LHC::Array)
+
+    n = size(LHC,1) #Num points
+    d = size(LHC,2) #Num dimensions
+    dist .= 0.0
+
+    # Squared l-2 norm of distances between all (unique) points
+    l = 0
+    for i = 2:n
+        for j = 1:i-1
+            l += 1
+            for k = 1:d
+                dist[l] += (LHC[i,k]-LHC[j,k])^2
+            end
+            dist[l] = 1/dist[l]
+        end
+    end
+
+    output = 1/(sum(dist))
+
+end
+
+
+
+"""
+    function AudzeEgliasObjective(LHC::Array)
+Same as AudzeEgliasObjective(dist,LHC::Array) but creating a new distance array.
+"""
+function AudzeEgliasObjective(LHC::Array)
+
+    n = size(LHC,1) #Num points
+    dist = zeros(Float64,Int(n*(n-1)*0.5))
+    AudzeEgliasObjective(dist,LHC::Array)
+
+end
+
+
+
+"""
+    function LHCoptim(n::Int,d::Int,gens;popsize::Int=100,ntour::Int=2,ptour=0.8)
+Produce an optimized Latin Hyper Cube with `d` dimensions and `n` sample points.
+Optimization is run for `gens` generations.
+"""
+function LHCoptim(n::Int,d::Int,gens;popsize::Int=100,ntour::Int=2,ptour=0.8)
+
+    #populate first individual
+    X = randomLHC(n,d)
+
+    LHCoptim!(X,gens,popsize=popsize,ntour=ntour,ptour=ptour)
+
+end
+
+
+
+"""
+    function LHCoptim!(X::Array{Int,2},n::Int,d::Int,gens;ntour::Int=2,ptour=0.8)
+Same as LHCoptim(n::Int,d::Int,gens;popsize::Int=100,ntour::Int=2,ptour=0.8) but using an
+existing population. Useful for continued optimization.
+"""
+function LHCoptim!(X::Array{Int,2},gens;popsize=100,ntour::Int=2,ptour=0.8)
+
+    #preallocate memory
+    n,d = size(X)
+    pop = Array{Int}(popsize,n,d)
+
+    pop[1,:,:] = X
+    for i = 2:popsize
+        pop[i,:,:] = randomLHC(n,d)
+    end
+
+
+    #preallocate memory
+    nextpop = Array{Int}(popsize,n,d)
+    fitness = Vector{Float64}(popsize)
+    bestfits = Array{Float64}(gens)
+    dist = zeros(Float64,Int(n*(n-1)*0.5))
+
+
+    #crossover for even population size
+    popEven = iseven(popsize)*-1
+
+
+    #dynamic mutation rate table
+    muts = Array{Int}(gens)
+    for l = 1:gens
+        muts[l] = round(Int,-n/(gens*0.75)*l+n)
+        if muts[l] < 1
+            muts[l] = 1
+        end
+    end
+
+
+    #populate first populations evaluate fitness
+    for i = 1:popsize
+        fitness[i] = AudzeEgliasObjective(dist, pop[i,:,:])
+    end
+
+
+    #save the best individual and it's fitness
+    bestfit, bestind = findmax(fitness)
+    nextpop[1,:,:] = pop[bestind,:,:]
+    bestfits[1] = bestfit
+
+
+    #iterate for gens generations
+    for k = 1:gens
+
+        #tournament selection
+        for i = 2:popsize
+            winnerInd = tournament(fitness,ntour,ptour)
+            nextpop[i,:,:] = pop[winnerInd,:,:]
+        end
+
+        #create children from crossover
+        for i = 2:2:popsize+popEven
+            for j = 1:d
+                if rand() < 1/d
+                    nextpop[i,:,j], nextpop[i+1,:,j] = fixedcross(pop[i,:,j],pop[i+1,:,j])
+                end
+            end
+        end
+
+        #perform inversion and mutation
+        for i = 2:popsize
+            for j = 1:d
+                if rand() < 1/d     #probability for inversion
+                    inversion!(view(nextpop,i,:,j))
+                end
+            end
+
+            for j=1:muts[gens]
+                mutateLHC!(view(nextpop,i,:,:))
+            end
+
+        end
+
+        #evaluate fitness
+        for i = 1:popsize
+            fitness[i] = AudzeEgliasObjective(dist, nextpop[i,:,:])
+        end
+
+        #set altered population to current
+        pop = copy(nextpop)
+
+        #set the first individual to the best and save the fitness
+        bestfit, bestind = findmax(fitness)
+        nextpop[1,:,:] = pop[bestind,:,:]
+        bestfits[k] = bestfit
+    end
+
+    return nextpop[1,:,:], bestfits
+
+end
+
+
+
+"""
+    function subLHCoptim(X,n::Int,gens;popsize::Int=100,ntour::Int=2,ptour=0.8)
+Produce an optimized Latin Hyper Cube with `n` sample points from a subset of
+points in `X`. Optimization is run for `gens` generations.
+"""
+function subLHCoptim(X,n::Int,gens;popsize::Int=100,ntour::Int=2,ptour=0.8)
+
+    #preallocate memory
+    nLarge, d = size(X)
+    pop = Array{Int}(popsize+1,n,d)
+    nextpop = Array{Int}(popsize+1,n,d)
+    fitness = Vector{Float64}(popsize+1)
+    bestfits = Array{Float64}(gens)
+    dist = zeros(Float64,Int(n*(n-1)*0.5))
+
+
+    #dynamic mutation rate table
+    muts = Array{Int}(gens)
+    for l = 1:gens
+        muts[l] = round(Int,-n/(gens*0.75)*l+n)
+        if muts[l] < 1
+            muts[l] = 1
+        end
+    end
+
+
+    #populate first population with random subLHC's and evaluate fitness
+    for i = 1:popsize+1
+        subInds = sample(1:nLarge, n, replace = false)
+        pop[i,:,:] = X[subInds,:]
+        fitness[i] = AudzeEgliasObjective(dist, pop[i,:,:])
+    end
+
+
+    #save the best individual and it's fitness
+    bestfit, bestind = findmax(fitness)
+    nextpop[1,:,:] = pop[bestind,:,:]
+    bestfits[1] = bestfit
+
+
+    #iterate for gens generations
+    for k = 1:gens
+
+        #tournament selection
+        for i = 2:popsize+1
+            winnerInd = tournament(fitness,ntour,ptour)
+            nextpop[i,:,:] = pop[winnerInd,:,:]
+        end
+
+        #perform mutation
+        for i = 2:popsize+1
+            for j=1:muts[gens]
+                subPoint = sample(1:n) #Randomly chosen point subLHC
+                largePoint = sample(1:nLarge) #Randomly chosen point largeLHC
+
+                while nextpop[i,subPoint,:] == X[largePoint,:]
+                    largePoint = sample(1:nLarge) #Choose new random point in largeLHC
+                end
+                nextpop[i,subPoint,:] = X[largePoint,:]
+            end
+        end
+
+        #evaluate fitness
+        for i = 1:popsize+1
+            fitness[i] = AudzeEgliasObjective(dist, nextpop[i,:,:])
+        end
+
+        #set altered population to current
+        pop = copy(nextpop)
+
+        #set the first individual to the best and save the fitness
+        bestfit, bestind = findmax(fitness)
+        nextpop[1,:,:] = pop[bestind,:,:]
+        bestfits[k] = bestfit
+    end
+
+    return nextpop[1,:,:], bestfits
+
+end
+
+
+
+"""
+    function subLHCindex(X,Xsub)
+Index in the large LHC to get the subLHC.
+"""
+function subLHCindex(X,Xsub)
+
+    nsub = size(Xsub,1)
+    subInds = Array{Int}(nsub)
+
+    for i = 1:nsub
+        subInds[i] = find(all(Xsub[i,:]' .== X, 2))[1]
+    end
+
+    return subInds
+end
+
+
+
+"""
+    function refineLHCoptim(X,gens;popsize::Int=100)
+Refine an existing plan by mutation only.
+"""
+function refineLHCoptim(X,gens;popsize::Int=100)
+
+    n, d = size(X)
+
+    #preallocate memory
+    pop = Array{Int}(popsize,n,d)
+    nextpop = Array{Int}(popsize,n,d)
+    fitness = Array{Float64}(popsize)
+    bestfits = Array{Float64}(gens)
+    dist = zeros(Float64,Int(n*(n-1)*0.5))
+
+    initFit = AudzeEgliasObjective(dist, X)
+    for i = 1:popsize
+        nextpop[i,:,:] = X
+        fitness[i] = initFit
+    end
+
+
+    #dynamic mutation rate table
+    muts = Array{Int}(gens)
+    for l = 1:gens
+        muts[l] = round(Int,-n/(gens*0.75)*l+n)
+        if muts[l] < 1
+            muts[l] = 1
+        end
+    end
+
+
+    #iterate for gens generations
+    for k = 1:gens
+
+        #perform mutation and evaluate fitness
+        for i = 2:popsize
+            for j=1:muts[gens]
+                mutateLHC!(view(nextpop,i,:,:))
+            end
+        end
+        for i = 1:popsize
+            fitness[i] = AudzeEgliasObjective(dist, nextpop[i,:,:])
+        end
+
+        #set altered population to current
+        pop = copy(nextpop)
+
+        #set the population to the best individual if the individuals fitness is
+        #below the threshold of the best
+        bestfit, bestind = findmax(fitness)
+        nextpop[1,:,:] = pop[bestind,:,:]
+        for i = 1:popsize
+            if fitness[i] < 0.98*bestfit
+                nextpop[i,:,:] = pop[bestind,:,:]
+            end
+        end
+        bestfits[k] = bestfit
+    end
+
+    return nextpop[1,:,:], bestfits
+
+end
+
 
 end # module
