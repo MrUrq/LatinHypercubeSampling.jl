49d63308fe27af31cdfa498ce251c30b30d8bc47
Magnus Urquhart <magnus.urquhart@gmail.com>
2018-09-27 14:32:46 +0200

    Updated docs requirement

--- docs/make.jl
+++ docs/make.jl
@@ -21,7 +21,7 @@ makedocs(
 deploydocs(
     repo = "github.com/MrUrq/LatinHypercubeSampling.jl.git",
     target = "build",
-    julia = "0.6",
+    julia = "1.0",
     deps = nothing,
     make = nothing
 )
