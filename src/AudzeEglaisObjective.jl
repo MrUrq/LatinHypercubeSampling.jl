b05e7051214e16270ac29726ea626f7a8ce1cd60
Yin Li <eelregit@gmail.com>
2020-03-08 19:19:37 -0400

    Add periodicity to the objective

--- src/AudzeEglaisObjective.jl
+++ src/AudzeEglaisObjective.jl
@@ -1,16 +1,17 @@
 @inline function _AudzeEglaisDist(LHC)
-    n,d = size(LHC) 
+    n,d = size(LHC)
     dist = 0.0
-    dist_tmp = 0.0
 
     # Squared l-2 norm of distances between all (unique) points
     for i = 2:n
         for j = 1:i-1
+            dist_tmp = 0.0
             for k = 1:d
-                @inbounds dist_tmp += (LHC[i,k]-LHC[j,k])^2
+                @inbounds dist_comp = LHC[i,k]-LHC[j,k]
+                dist_comp -= round(dist/n) * n
+                dist_tmp += dist_comp^2
             end
             @inbounds dist += 1/dist_tmp
-            dist_tmp = 0.0
         end
     end
     output = 1/dist
@@ -18,13 +19,13 @@
 end
 
 function _AudzeEglaisObjective(dim::Continuous,LHC)
-    output = _AudzeEglaisDist(LHC)    
+    output = _AudzeEglaisDist(LHC)
     return output
 end
 
 function _AudzeEglaisObjective(dim::Categorical,LHC)
-    output = _AudzeEglaisDist(LHC)     
-    output == Inf ? 0 : output    
+    output = _AudzeEglaisDist(LHC)
+    output == Inf ? 0 : output
 end
 
 """
@@ -36,7 +37,7 @@ Audze-Eglais distance which normally is minimized.
 function AudzeEglaisObjective(LHC::T; dims::Array{V,1} =[Continuous() for i in 1:size(LHC,2)],
                                             interSampleWeight::Float64=1.0,
                                             ) where T <: AbstractArray where V <: LHCDimension
-    
+
     out = 0.0
 
     #Compute the objective function among all points
@@ -46,7 +47,7 @@ function AudzeEglaisObjective(LHC::T; dims::Array{V,1} =[Continuous() for i in 1
     categoricalDimInds = findall(x->typeof(x)==Categorical,dims)
     for i in categoricalDimInds
         for j = 1:dims[i].levels
-            subLHC = @view LHC[LHC[:,i] .== j,:] 
+            subLHC = @view LHC[LHC[:,i] .== j,:]
             out += _AudzeEglaisObjective(dims[i],subLHC)*dims[i].weight
         end
     end
